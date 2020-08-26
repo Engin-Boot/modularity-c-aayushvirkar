85af0c493ee527c766461ad2f42219441965c4a0
aayushvirkar <51874905+aayushvirkar@users.noreply.github.com>
2020-08-26 09:04:31 +0530

    Update Program.cs

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -8,8 +8,8 @@ namespace TelCo.ColorCoder
     class Program
     {
         
-        public static Color[] colorMapMajor;
-        public static Color[] colorMapMinor;
+        internal static Color[] colorMapMajor;
+        internal static Color[] colorMapMinor;
         static Program()
         {
             colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
